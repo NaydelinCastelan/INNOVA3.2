73f20d03b5feda9da6f28c1356f0df2d44c5f41d
Lizbeth0911 <laura_1320114088@uptecamac.edu.mx>
2022-07-28 00:56:04 -0500

    actualizacion de vista

--- resources/views/Ventas/index.blade.php
+++ resources/views/Ventas/index.blade.php
@@ -14,18 +14,19 @@
                         <div class="row">
                             <div class="col-12 col-sm-6 col-md-4 mb-5">
                                 <a href="{{ route('pendientes') }}" target="_blank">
-                                    <img src="/assets/images/pendientes.png" alt="Plan Form" class="img-thumbnail">
+                                    <img src="/assets/images/crear.png" alt="Plan Form" class="img-thumbnail">
                                     <h4 class="d-block mt-3 mb-0 center">Pedidos pendientes</h4>
                                 </a>
                             </div>
                             <div class="col-12 col-sm-6 col-md-4 mb-5">
                                 <a href="{{ route('completados') }}" target="_blank">
-                                    <img src="/assets/images/completadas.png" alt="Floating Contact Form" class="img-thumbnail">
+                                    <img src="/assets/images/crear.png" alt="Floating Contact Form" class="img-thumbnail">
                                     <h4 class="d-block mt-3 mb-0 center">Pedidos completados</h4>
                                 </a>
                             </div>
                             <div class="col-12 col-sm-6 col-md-4 mb-5">
-                                <a href="forms-booking-reservation.html" target="_blank">
+                                <a href="{{ route('generar') }}"
+                                 target="_blank">
                                     <img src="/assets/images/crear.png" alt="Booking Reservation Form"
                                         class="img-thumbnail">
                                     <h4 class="d-block mt-3 mb-0 center">Generar pedido</h4>
